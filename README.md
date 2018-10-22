dc5a9b91a6ef295a2f2898a445274dbeb9ff1c5e
chian88 <leechianyee@gmail.com>
2018-10-22 14:08:54 +0800

    aok

---
+++ README.md
@@ -0,0 +1,30 @@
+implement literally canvas 
+
+use bootstrap for its columns. 
+
+buttons to use.
+- line - icon arrow
+- rectangle - icon square
+- triangle - 
+- curve line - pen icon 
+- circle - icon circle.
+- eraser - icon eraser
+- text  - icon A
+- clear - icon clear
+- move - icon move
+
+
+styling 
+- width
+- undo / forward
+- move
+- fill 
+- stroke
+- filter (brightness)
+- brush
+
+
+extra features
+Size up size down touch
+Mobile support - touch event
+Exporting drawings to PNG and SVG
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,34 @@
 $(function() {
-	$("#circle").on("click", function() {
+	$("#circle").on("click", function(e) {
 		$("#topbar").children().hide();
-		$("#topbar").append($("#thickness").html());
+		$("#topbar #thickness").show();
+		
 	}); 
 
-	$("#font").on("click", function() {
+	$("#font").on("click", function(e) {
 		$("#topbar").children().hide();
-		$("#topbar").append($("#font-option").html());
+		$("#topbar #font-option").show();
 	})
+
+	$(".sidebar").on("click", "div[id]", function(e) {
+		// debugger;
+		$div = $(e.currentTarget);
+		$sidebar = $div.closest(".sidebar");
+		$sidebar.find("div[id]").removeClass("active");
+		$div.toggleClass("active");
+	});
+
+	$("div#background").on("click", function(e) {
+		debugger;
+		var $event = $(e.target);
+		var color = extractColorFromEvent($event);
+
+		$("#canvas").css("background-color", color);
+
+	});
+
+
+	function extractColorFromEvent($event) {
+		return $event.attr('style').split(":")[1].trim().replace(";", '');
+	}
 })
